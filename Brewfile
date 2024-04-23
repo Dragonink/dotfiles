f184f1bc15faa1017fd3f4bf858bd7d1a4652263
Tanguy Berthoud <tanguy.berthoud@orolia2s.com>
2024-04-23 20:04:11 +0200

    bruh

--- Brewfile
+++ Brewfile
@@ -1,9 +1,16 @@
 tap "homebrew/bundle"
+tap "homebrew/linux-fonts"
 brew "bash-completion@2"
 brew "bat"
 brew "btop"
 brew "eza"
 brew "fzf"
 brew "forgit"
+brew "helix", args: ["HEAD"]
 brew "starship"
 brew "stow"
+brew "homebrew/linux-fonts/font-fira-code"
+brew "homebrew/linux-fonts/font-noto-emoji", args: ["HEAD"]
+brew "homebrew/linux-fonts/font-noto-mono", args: ["HEAD"]
+brew "homebrew/linux-fonts/font-noto-sans"
+brew "homebrew/linux-fonts/font-noto-serif"
--- kitty/dot-bashrc.d/kitty.bash
+++ kitty/dot-bashrc.d/kitty.bash
@@ -6,7 +6,7 @@ export KITTY_SHELL_INTEGRATION='enabled'
 source "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"
 
 # Alias commands with kitty integrations
-alias diff='kitten diff'
-alias ssh='kitten ssh'
-alias icat='kitten icat'
+alias diff='kitty +kitten diff'
+alias ssh='kitty +kitten ssh'
+alias icat='kitty +kitten icat'
 alias idot='dot -Gbgcolor=#00000000 -Gcolor=white -Gfontcolor=white -Ncolor=white -Nfontcolor=white -Ecolor=white -T png | icat'
