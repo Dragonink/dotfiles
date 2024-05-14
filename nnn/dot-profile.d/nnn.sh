b353da0a8dde51af069515e5a779ffaee91edd28
Dragonink <Dragonink@users.noreply.github.com>
2024-05-14 22:40:04 +0200

    Remove nnn

--- nnn/dot-profile.d/nnn.sh
+++
@@ -1,8 +0,0 @@
-# Configure nnn options
-export NNN_OPTS='aeoUx'
-# Make nnn use trash-cli
-export NNN_TRASH=1
-# Define bookmarks
-export NNN_BMS="/:/;h:$HOME"
-# Define plugins
-export NNN_PLUG='s:!bash -i*;m:!bashmount'
