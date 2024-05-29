e7e36b73deefc77db1c228d7f3af52bd66e6f004
Dragonink <Dragonink@users.noreply.github.com>
2024-05-29 18:10:46 +0200

    Remove yazi

--- yazi/dot-config/yazi/plugins/arrow.yazi/init.lua
+++
@@ -1,8 +0,0 @@
-local function entry(st, args)
-	local current = cx.active.current
-	local new = (current.cursor + args[1]) % #current.files
-
-	ya.manager_emit("arrow", { new - current.cursor })
-end
-
-return { entry = entry }
