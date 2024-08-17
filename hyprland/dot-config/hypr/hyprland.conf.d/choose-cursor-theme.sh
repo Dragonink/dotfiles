793792f7c1b191d77fa6b1b36ad8a45d3b38b07a
Dragonink <Dragonink@users.noreply.github.com>
2024-08-17 11:54:41 +0200

    Send notification when choosing cursor theme

--- hyprland/dot-config/hypr/hyprland.conf.d/choose-cursor-theme.sh
+++ hyprland/dot-config/hypr/hyprland.conf.d/choose-cursor-theme.sh
@@ -9,4 +9,7 @@ if [ -n "$XCURSOR_THEME" ]; then
 	hyprctl setcursor "$XCURSOR_THEME" 24
 	# Apply to theme to GTK
 	gsettings set org.gnome.desktop.interface cursor-theme "$XCURSOR_THEME"
+
+	# Notify user
+	hyprctl notify 1 3000 0 "Cursor theme is $XCURSOR_THEME"
 fi
