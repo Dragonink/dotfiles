8cdd9e898b045905b412ce8435ce5b38ebceccdb
Tanguy Berthoud <tanguy.berthoud@orolia2s.com>
2024-09-11 15:03:40 +0200

    Fix XDG

--- gum/dot-config/bashrc.d/gum.bash
+++ gum/dot-config/bashrc.d/gum.bash
@@ -1,5 +1,2 @@
-# Use gum as the pager for bat
-export BAT_PAGER='gum pager --show-line-numbers=false'
-
 # Make sudo use gum
 export SUDO_ASKPASS="$HOME/.local/bin/gum-askpass"
--- xdg/dot-config/profile.d/00-xdg.sh
+++ xdg/dot-config/profile.d/00-xdg.sh
@@ -1,23 +1,15 @@
-# Define XDG base directories
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_STATE_HOME="$HOME/.local/state"
-
-# Extend PATH
-export PATH="$HOME/.local/bin:$PATH"
-
 # Define XDG user directories
 #shellcheck disable=SC1091
 . "$XDG_CONFIG_HOME/user-dirs.dirs"
-export XDG_DESKTOP_DIR
-export XDG_DOWNLOAD_DIR
-export XDG_TEMPLATES_DIR
-export XDG_PUBLICSHARE_DIR
-export XDG_DOCUMENTS_DIR
-export XDG_MUSIC_DIR
-export XDG_PICTURES_DIR
-export XDG_VIDEOS_DIR
+export \
+	XDG_DESKTOP_DIR \
+	XDG_DOWNLOAD_DIR \
+	XDG_TEMPLATES_DIR \
+	XDG_PUBLICSHARE_DIR \
+	XDG_DOCUMENTS_DIR \
+	XDG_MUSIC_DIR \
+	XDG_PICTURES_DIR \
+	XDG_VIDEOS_DIR
 
 # Export XDG environment variables to systemd
 dbus-update-activation-environment --systemd \
