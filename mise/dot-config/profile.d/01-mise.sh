9172b36cf7895529f93294b426ef29631a8e212d
Tanguy Berthoud <tanguy.berthoud@orolia2s.com>
2024-09-13 11:54:38 +0200

    Fix mise PATH

--- mise/dot-config/profile.d/01-mise.sh
+++ mise/dot-config/profile.d/01-mise.sh
@@ -1,6 +1,6 @@
+# Add mise shims to PATH
+eval "$(mise activate bash --shims)"
+
 # Make mise use all CPU cores
 MISE_JOBS="$(nproc)"
 export MISE_JOBS
-
-# Add mise shims to PATH
-eval "$(mise activate bash --shims)"
