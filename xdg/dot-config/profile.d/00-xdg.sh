e1d1a4d7045b21aa65afc9e2ece4d4bf7cf7a29f
Dragonink <Dragonink@users.noreply.github.com>
2024-09-28 10:59:55 +0200

    Move dbus-update-activation-environment

--- xdg/dot-config/profile.d/00-xdg.sh
+++ xdg/dot-config/profile.d/00-xdg.sh
@@ -11,20 +11,5 @@ export \
 	XDG_PICTURES_DIR \
 	XDG_VIDEOS_DIR
 
-# Export XDG environment variables to systemd
-dbus-update-activation-environment --systemd \
-	XDG_CONFIG_HOME \
-	XDG_CACHE_HOME \
-	XDG_DATA_HOME \
-	XDG_STATE_HOME \
-	XDG_DESKTOP_DIR \
-	XDG_DOWNLOAD_DIR \
-	XDG_TEMPLATES_DIR \
-	XDG_PUBLICSHARE_DIR \
-	XDG_DOCUMENTS_DIR \
-	XDG_MUSIC_DIR \
-	XDG_PICTURES_DIR \
-	XDG_VIDEOS_DIR
-
 # Make GTK applications use the XDG portal
 export GTK_USE_PORTAL=1
