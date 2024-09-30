2c60cd41f814c7b8d84fe18452cce9d34699566f
Tanguy Berthoud <tanguy.berthoud@orolia2s.com>
2024-09-30 11:42:22 +0200

    CARGO_HOME

--- devel/dot-config/bashrc.d/devel.bash
+++ devel/dot-config/bashrc.d/devel.bash
@@ -3,8 +3,9 @@ MAKEFLAGS="--jobs=$(nproc)"
 export MAKEFLAGS
 
 # Rust
-if [ -f "${CARGO_HOME:-$HOME/.cargo}/env" ]; then
+export CARGO_HOME="$HOME/.cargo"
+if [ -f "$CARGO_HOME/env" ]; then
 	#shellcheck disable=SC1091
-	source "${CARGO_HOME:-$HOME/.cargo}/env"
+	source "$CARGO_HOME/env"
 fi
 export CLIPPY_CONF_DIR="$XDG_CONFIG_HOME/clippy"
