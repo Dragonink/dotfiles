7528deca07dbb4d16e8cf16ad4e50f1bd144b973
Dragonink <Dragonink@users.noreply.github.com>
2025-02-18 11:45:37 +0100

    Move Git difftool kitty to kitty's configuration script

--- dot_config/kitty/executable_kitty.conf.sh
+++ dot_config/kitty/executable_kitty.conf.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
 
 # Use Nushell
 if nu="$(command -v nu)"; then
   echo "shell $nu"
   echo "env SHELL=$nu"
 fi
+
+# Configure Git to use the diff kitten
+GIT_CONFIG_COUNT="${GIT_CONFIG_COUNT:-0}"
+function git_config() {
+  echo "env GIT_CONFIG_KEY_$GIT_CONFIG_COUNT=$1"
+  echo "env GIT_CONFIG_VALUE_$GIT_CONFIG_COUNT=$2"
+
+  GIT_CONFIG_COUNT="$(($GIT_CONFIG_COUNT + 1))"
+}
+git_config 'difftool.kitty.cmd' 'kitten diff $LOCAL $REMOTE'
+git_config 'alias.difftool-kitty' 'difftool --tool=kitty --dir-diff --no-symlinks'
+echo "env GIT_CONFIG_COUNT=$GIT_CONFIG_COUNT"
