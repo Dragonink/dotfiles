260101fce7fdce3544b4f070bd0b62499a1c4397
Dragonink <Dragonink@users.noreply.github.com>
2026-04-17 17:08:00 +0200

    feat(ssh): Use GPG agent in Bash

---
+++ ssh/rc.bash.d/75-ssh-agent.bash
@@ -0,0 +1,8 @@
+#shellcheck shell=bash
+
+# Use GPG agent as SSH agent
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+	SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+	export SSH_AUTH_SOCK
+fi
