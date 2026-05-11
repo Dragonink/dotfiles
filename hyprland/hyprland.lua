6c7794e400634e78a9f9638a43e3015ae1b61a6e
Dragonink <Dragonink@users.noreply.github.com>
2026-05-11 16:46:28 +0200

    feat(hyprland): Enforce permissions
    Deny unknown keyboards for security. Display a warning if no keyboards
    are allowed.

--- hyprland/hyprland.lua
+++ hyprland/hyprland.lua
@@ -9,6 +9,7 @@ function request(modname)
 	end
 end
 
+ALLOWED_KEYBOARDS = {}
 request("hyprland.local")
 
 
@@ -51,3 +52,19 @@ hl.config({
 		no_donation_nag = true,
 	},
 })
+
+-- PERMISSIONS
+hl.config({ ecosystem = { enforce_permissions = true } })
+-- Bypass XDG Desktop Portal for screen copy
+hl.permission({ binary = "/usr/bin/grim", type = "screencopy", mode = "allow" })
+hl.permission({ binary = "/usr/bin/hyprlock", type = "screencopy", mode = "allow" })
+-- Deny unknown keyboards
+for _, kb in ipairs(ALLOWED_KEYBOARDS) do
+	hl.permission({ binary = kb, type = "keyboard", mode = "allow" })
+end
+if #ALLOWED_KEYBOARDS > 0 then
+	hl.permission({ binary = ".*", type = "keyboard", mode = "deny" })
+else
+	hl.permission({ binary = ".*", type = "keyboard", mode = "allow" })
+	hl.notification.create({ icon = 0, duration = 10e3, text = "All keyboards are allowed! Define ALLOWED_KEYBOARDS in local.lua" })
+end
