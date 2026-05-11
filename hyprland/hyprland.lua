cc7c86c36efd58d5a8e21325a8ced515345797ae
Dragonink <Dragonink@users.noreply.github.com>
2026-05-11 16:09:40 +0200

    feat(hyprland): Add base config

---
+++ hyprland/hyprland.lua
@@ -0,0 +1,48 @@
+function request(modname)
+	if package.searchpath(modname, package.path) then
+		return require(modname)
+	end
+end
+
+request("hyprland.local")
+
+
+-- Window swallowing
+hl.config({
+	misc = {
+		enable_swallow = true,
+		swallow_regex = "com\\.mitchellh\\.ghostty",
+	},
+})
+
+-- Quickly configure unkown monitors
+hl.monitor({
+	output = "",
+	mode = "preferred",
+	position = "auto",
+	scale = "auto",
+	cm = "auto",
+})
+
+-- RENDER
+hl.config({
+	render = {
+		new_render_scheduling = true,
+	},
+	misc = {
+		vrr = 2, -- Fullscreen only
+	},
+	xwayland = {
+		force_zero_scaling = true,
+	},
+})
+
+-- MISCELLANEOUS
+hl.config({
+	misc = {
+		disable_autoreload = true,
+	},
+	ecosystem = {
+		no_donation_nag = true,
+	},
+})
