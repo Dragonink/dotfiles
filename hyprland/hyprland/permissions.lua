4b8f5c1d40e8248dfa9d2f05df97f36af0edb7af
Dragonink <Dragonink@users.noreply.github.com>
2026-07-21 18:53:45 +0200

    feat(hyprland): Deny unknown keyboards

--- hyprland/hyprland/permissions.lua
+++ hyprland/hyprland/permissions.lua
@@ -1,1 +1,12 @@
 hl.config({ ecosystem = { enforce_permissions = true } })
+
+-- Deny unknown keyboards
+for _, kb in ipairs(ALLOWED_KEYBOARDS) do
+	hl.permission({ binary = kb, type = "keyboard", mode = "allow" })
+end
+if #ALLOWED_KEYBOARDS > 0 then
+	hl.permission({ binary = ".*", type = "keyboard", mode = "deny" })
+else
+	hl.permission({ binary = ".*", type = "keyboard", mode = "allow" })
+	hl.notification.create({ icon = "warning", font_size = 24, text = "All keyboards are allowed!\nDefine ALLOWED_KEYBOARDS in local.lua", timeout = 20e3 })
+end
