ffc84ed2e2bac8e9bf2ec44b531126483e7524db
Dragonink <Dragonink@users.noreply.github.com>
2026-07-21 18:54:19 +0200

    feat(hyprland): Bypass XDPH for screen copy

--- hyprland/hyprland/permissions.lua
+++ hyprland/hyprland/permissions.lua
@@ -10,3 +10,11 @@ else
 	hl.permission({ binary = ".*", type = "keyboard", mode = "allow" })
 	hl.notification.create({ icon = "warning", font_size = 24, text = "All keyboards are allowed!\nDefine ALLOWED_KEYBOARDS in local.lua", timeout = 20e3 })
 end
+
+-- Bypass XDG Desktop Portal for screen copy
+for _, bin in ipairs({
+	"/usr/lib/xdg-desktop-portal-hyprland",
+	"/usr/bin/grim",
+}) do
+	hl.permission({ binary = bin, type = "screencopy", mode = "allow" })
+end
