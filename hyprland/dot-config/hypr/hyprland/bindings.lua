fee999140365d22d75f0cdf37d812fa21c36518b
Dragonink <Dragonink@users.noreply.github.com>
2026-09-19 12:15:12 +0200

    feat(hyprland): Add bindings to move focus

--- hyprland/dot-config/hypr/hyprland/bindings.lua
+++ hyprland/dot-config/hypr/hyprland/bindings.lua
@@ -3,4 +3,8 @@ require("hyprland.bindings.define")
 hl.bind("SUPER+Return", hl.dsp.exec_raw("app2unit -T"), { description = "Open new terminal" })
 hl.bind("SUPER+Space", hl.dsp.exec_raw("fuzzel"), { description = "Open application launcher" })
 
+for _, direction in pairs(DIRECTIONS) do
+	hl.bind("SUPER+"..direction.key, hl.dsp.focus({ direction = direction.dispatch }), { description = "Move focus "..tostring(direction) })
+end
+
 require("hyprland.bindings.window")
