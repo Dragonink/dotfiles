9228b6a28d2c658cd07ad53ef455595b76d8e838
Dragonink <Dragonink@users.noreply.github.com>
2026-09-19 12:15:12 +0200

    feat(hyprland): Load local file if present

--- hyprland/dot-config/hypr/hyprland.lua
+++ hyprland/dot-config/hypr/hyprland.lua
@@ -1,3 +1,13 @@
+--- Loads the given module if it can be found.
+---@param modname string
+---@see require
+---@diagnostic disable-next-line:lowercase-global
+function require_optional(modname)
+	if package.searchpath(modname, package.path) then
+		return require(modname)
+	end
+end
+
 hl.config({
 	misc = {
 		disable_autoreload = true,
@@ -7,3 +17,5 @@ hl.config({
 		no_donation_nag = true,
 	},
 })
+
+require_optional("hyprland.local")
