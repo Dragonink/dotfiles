c9ee952e893b4fc57c096940c9885a1cbdbecabe
Dragonink <Dragonink@users.noreply.github.com>
2026-09-19 12:15:12 +0200

    feat(hyprland): Add bindings to move windows

--- hyprland/dot-config/hypr/hyprland/bindings/window.lua
+++ hyprland/dot-config/hypr/hyprland/bindings/window.lua
@@ -1,2 +1,7 @@
 hl.bind("SUPER+Q", hl.dsp.window.close(), { description = "Close window" })
 hl.bind("SUPER+SHIFT+Q", hl.dsp.window.kill(), { description = "Kill window" })
+
+for _, direction in pairs(DIRECTIONS) do
+	hl.bind("SUPER+SHIFT+"..direction.key, hl.dsp.window.move({ direction = direction.dispatch }), { description = "Move window "..tostring(direction) })
+end
+hl.bind("SUPER+"..LMB, hl.dsp.window.drag(), { mouse = true, description = "Drag window" })
