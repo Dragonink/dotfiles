186b1ac130938a7a69bd254f186ed56752718864
Dragonink <Dragonink@users.noreply.github.com>
2026-09-19 12:15:12 +0200

    chore(hyprland): Provide consts for mouse keys

--- hyprland/dot-config/hypr/hyprland/bindings.lua
+++ hyprland/dot-config/hypr/hyprland/bindings.lua
@@ -1,5 +1,10 @@
 require("hyprland.bindings.define")
 
+--- Key code for the Left Mouse Button
+LMB = "mouse:272"
+--- Key code for the Right Mouse Button
+RMB = "mouse:273"
+
 hl.bind("SUPER+Return", hl.dsp.exec_raw("app2unit -T"), { description = "Open new terminal" })
 hl.bind("SUPER+Space", hl.dsp.exec_raw("fuzzel"), { description = "Open application launcher" })
 
