a284e6c34bce7d474428b9d9a8513fb866022aba
Dragonink <Dragonink@users.noreply.github.com>
2026-09-19 12:15:12 +0200

    feat(hyprland): Add bindings to close windows

--- hyprland/dot-config/hypr/hyprland/bindings.lua
+++ hyprland/dot-config/hypr/hyprland/bindings.lua
@@ -1,2 +1,4 @@
 hl.bind("SUPER+Return", hl.dsp.exec_raw("app2unit -T"), { description = "Open new terminal" })
 hl.bind("SUPER+Space", hl.dsp.exec_raw("fuzzel"), { description = "Open application launcher" })
+
+require("hyprland.bindings.window")
---
+++ hyprland/dot-config/hypr/hyprland/bindings/window.lua
@@ -0,0 +1,2 @@
+hl.bind("SUPER+Q", hl.dsp.window.close(), { description = "Close window" })
+hl.bind("SUPER+SHIFT+Q", hl.dsp.window.kill(), { description = "Kill window" })
